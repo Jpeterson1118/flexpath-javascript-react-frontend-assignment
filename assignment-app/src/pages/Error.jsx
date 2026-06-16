efdb42fe6c92974eea2a257901ceabdaf1117f81
Jpeterson <peterson.jacob13@protonmail.com>
2026-06-16 14:32:42 -0500

    corrected className propterty in error.jsx

--- assignment-app/src/pages/Error.jsx
+++ assignment-app/src/pages/Error.jsx
@@ -1,5 +1,5 @@
 export default function ErrorPage () {
-    <div class="d-flex justify-content-center">
+    <div className="d-flex justify-content-center">
         <h1>404: Page Not Found</h1>
     </div>
 }
